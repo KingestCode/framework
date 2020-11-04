395d35a1bc1ab213ac97fa6fa0a385cc994fba45
miaoqi <363962900@qq.com>
2020-11-04 18:19:02 +0800

    update blog

---
+++ miaoqi-spring-boot-apollo/src/main/java/com/miaoqi/miaoqi/apollo/miaoqispringbootapollo/MiaoqiSpringBootApolloApplication.java
@@ -0,0 +1,13 @@
+package com.miaoqi.miaoqi.apollo.miaoqispringbootapollo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiaoqiSpringBootApolloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiaoqiSpringBootApolloApplication.class, args);
+	}
+
+}
---
+++ miaoqi-spring-boot-apollo/src/test/java/com/miaoqi/miaoqi/apollo/miaoqispringbootapollo/MiaoqiSpringBootApolloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miaoqi.miaoqi.apollo.miaoqispringbootapollo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiaoqiSpringBootApolloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
